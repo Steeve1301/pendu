7c0fcd00631ffec8c1df4940b2ae9da4aab4c3ef
Steeve1301 <steevehenrard5@gmail.com>
2020-02-06 16:51:09 +0100

    Pendu

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let word= ['b', 'o', 'n', 'j', 'o', 'u','r'];
+
+    alert("Bravo vous avez gagné! Si vous voulez réessayer, appuyer sur F5");
+    document.getElementById("putletter").addEventListener("click", () =>{
+        let letter= document.getElementById("letter").value;    
+        return letter;
+    });    
+
