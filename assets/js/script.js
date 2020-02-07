18a9644c664283c847ef1eac551257049acc2097
Steeve1301 <steevehenrard5@gmail.com>
2020-02-07 12:00:04 +0100

    Update Pendu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,31 @@
 let word= ['b', 'o', 'n', 'j', 'o', 'u','r'];
+let findword= ['_','_','_','_','_','_','_'];
+let position=0;
+let try_game= 6;
+let playing= true;
 
-    alert("Bravo vous avez gagné! Si vous voulez réessayer, appuyer sur F5");
-    document.getElementById("putletter").addEventListener("click", () =>{
-        let letter= document.getElementById("letter").value;    
-        return letter;
-    });    
+document.getElementById("find_word").innerHTML= findword.join(' ');
+
+document.getElementById("putletter").addEventListener("click", () =>{
+    letter= document.getElementById("letter").value;    
+    if(playing==true){
+        for(let i=0; i<word.length; i++){
+            if(letter== word[i]){
+                position= i;
+                console.log(position);
+                findword.splice(i,1,letter);
+                document.getElementById("find_word").innerHTML= findword.join(' ');
+            }
+            if(try_game == 0){
+                playing=false;
+            }
+        }
+    }
+    
+    else{
+        document.getElementById("find_word").innerHTML= "Désolé, vous avez perdu!";
+    }
+
+});    
+
 
